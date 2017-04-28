e9df06d56072ce4e03f0f84f0344a09348e72a48
Gabriel Russell <TheBlargbla@gmail.com>
2017-04-28 10:57:37 -0400

    Main.c Comments
    I'm going through the HelloEntryPoint repo, and trying to understand exactly what happens. I have a minimal knowledge of C, so I'll be chipping it piece by piece. Eventually, I'll move to editing the contents and trying other stuff.

--- source/main.c
+++ source/main.c
@@ -2,8 +2,9 @@
 #include "draw.h"
 #include "hid.h"
 #include "i2c.h"
-
+// Constants
 #define DISPLAY_TEXT "Hello ARM9 Entrypoint!"
+// Starting locations for cursor
 #define START_X 10
 #define START_Y 10
 
@@ -25,26 +26,29 @@ void PowerOff()
 int main()
 {
     char clear_text[64] = { 0 };
-    bool use_top = true;
-    u32 cur_x = START_X;
+    bool use_top = true; //Use top screen
+    //Unsigned 32 bit ints start with the value of our constants.
+    u32 cur_x = START_X; 
     u32 cur_y = START_Y;
     
-    memset(clear_text, (int) ' ', strnlen(DISPLAY_TEXT, 63));
+    memset(clear_text, (int) ' ', strnlen(DISPLAY_TEXT, 63)); //Set clear_text to the value " " for the length of DISPLAY_TEXT constant. Basically clears the screen
     
     while( true ) {
-        DrawStringF(cur_x, cur_y, use_top, DISPLAY_TEXT);
+        DrawStringF(cur_x, cur_y, use_top, DISPLAY_TEXT); //Write the string (constant) DISPLAY_TEXT at the location of the cursor.
         u32 pad_state = InputWait();
-        u32 increment = 1;
-        DrawStringF(cur_x, cur_y, use_top, clear_text);
+        u32 increment = 1; //Speed of cursor
+        DrawStringF(cur_x, cur_y, use_top, clear_text); //Clear screen after no input or time?
         if (pad_state & (BUTTON_B | BUTTON_START))
-            break;
+            break; //Leave after start or press b.
         if (pad_state & BUTTON_L1)
-            increment = 10;
+            increment = 10; //Speed Up
         if (pad_state & BUTTON_R1) {
             cur_x = START_X;
             cur_y = START_Y;
             use_top = !use_top;
+            //Reset / Stop??
         }
+        //Move cursor
         if (pad_state & BUTTON_UP)
             cur_y = (cur_y >= increment) ? cur_y - increment : 0;
         else if (pad_state & BUTTON_LEFT)
