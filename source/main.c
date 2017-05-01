2a33a4ded52ccd04b28c83da0d9471b258ae032c
Gabriel Russell <TheBlargbla@gmail.com>
2017-05-01 13:03:10 -0400

    Small Update
    Questions, and things. I'm still looking at this.

--- source/main.c
+++ source/main.c
@@ -3,7 +3,7 @@
 #include "hid.h"
 #include "i2c.h"
 // Constants
-#define DISPLAY_TEXT "Hello ARM9 Entrypoint!"
+#define DISPLAY_TEXT "A9LH / Brahma payload!" //Theoretically, I should be able to change this message to anything else. I still haven't built it myself. (5/1/2017)
 // Starting locations for cursor
 #define START_X 10
 #define START_Y 10
@@ -27,15 +27,18 @@ int main()
 {
     char clear_text[64] = { 0 };
     bool use_top = true; //Use top screen
+    
     //Unsigned 32 bit ints start with the value of our constants.
     u32 cur_x = START_X; 
     u32 cur_y = START_Y;
     
     memset(clear_text, (int) ' ', strnlen(DISPLAY_TEXT, 63)); //Set clear_text to the value " " for the length of DISPLAY_TEXT constant. Basically clears the screen
     
+    //I notice that the char clear_text has a length of 64, while this has a 63 stuck in here. Does this have anything to do with it? I'll experiment later.
+    
     while( true ) {
         DrawStringF(cur_x, cur_y, use_top, DISPLAY_TEXT); //Write the string (constant) DISPLAY_TEXT at the location of the cursor.
-        u32 pad_state = InputWait();
+        u32 pad_state = InputWait(); // Wait for input before proceeding?
         u32 increment = 1; //Speed of cursor
         DrawStringF(cur_x, cur_y, use_top, clear_text); //Clear screen after no input or time?
         if (pad_state & (BUTTON_B | BUTTON_START))
